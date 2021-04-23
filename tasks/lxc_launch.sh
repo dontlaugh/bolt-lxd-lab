3e6831b1710172eb82ba9eeed055006714a40753
Coleman McFarland <coleman@de.limited>
2021-04-23 11:53:33 -0400

    enhance launch task

--- tasks/lxc_launch.sh
+++ tasks/lxc_launch.sh
@@ -2,5 +2,7 @@
 
 image=$PT_image
 name=$PT_name
+optional_args=$PT_args
 
-lxc launch $image $name
+# redirect stderr, because bolt only displays a single stream
+lxc launch $image $name $optional_args 2>&1
